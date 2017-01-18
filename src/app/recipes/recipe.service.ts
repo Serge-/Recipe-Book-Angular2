a4b67154c91d168633a7a408b1478ec627534300
Serge- <enin.serge@gmail.com>
2017-01-18 15:11:19 +0100

    Recipe Service, Firebase data connect

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
 import { Recipe } from './recipe'
 import {Ingredient} from "../shared/ingredient";
+import {Headers, Http, Response} from "@angular/http";
+import 'rxjs/Rx';
+
 
 @Injectable()
 export class RecipeService {
+  recipeChanged = new EventEmitter<Recipe[]>();
+
   private  recipes: Recipe[] = [
     new Recipe('Schnitzel', 'Vary Tasty', 'http://www.paramountbutchershopusa.com/assets/uploads/tray_item_images/1435171328.png',[
         new Ingredient('French Fries', 2),
@@ -11,7 +16,7 @@ export class RecipeService {
     ]),
     new Recipe('Salad', 'Vary Healthy', 'http://brownbagonline.com/wp-content/uploads/2013/07/MissMaeSalad.png',[])
   ];
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes
@@ -27,5 +32,22 @@ export class RecipeService {
   }
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return  this.http.put('https://recipeapp-808e5.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+  fetchData() {
+    return this.http.get('https://recipeapp-808e5.firebaseio.com/recipes.json')
+        .map((response: Response) => response.json())
+        .subscribe(
+            (data: Recipe[]) => {
+              this.recipes = data;
+              this.recipeChanged.emit(this.recipes);
+            }
+        );
   }
 }
